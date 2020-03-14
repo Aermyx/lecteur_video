33c499c211d7ce1ee034878b4f65e10213f86cbd
Aermyx <54551414+Aermyx@users.noreply.github.com>
2020-03-13 23:30:08 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,21 @@ var btnPlay = document.querySelector(".play_block");
 var btnPause = document.querySelector(".pause_block");
 var btnStop = document.querySelector(".stop_block");
 
+btnPlay.addEventListener("click",function(){
+    video.play();
+    btnPause.src = "https://imgur.com/Us8Tqmc.png";
+    btnPlay.src = "https://imgur.com/C1zPEYA.png";
+});
+
+btnPause.addEventListener("click",function(){
+    video.pause();
+    btnPlay.src = "https://imgur.com/H4GywUm.png";
+    btnPause.src = "https://imgur.com/C1zPEYA.png";
+});
+
+btnStop.addEventListener("click",function(){
+    video.currentTime = 0;
+    video.pause();
+    btnPlay.src = "https://imgur.com/H4GywUm.png";
+    btnPause.src = "https://imgur.com/C1zPEYA.png";
+});
