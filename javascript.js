5c3b2771e27b3c228fb1ed75d1c2ced20ffa4879
Aermyx <54551414+Aermyx@users.noreply.github.com>
2020-03-13 22:20:17 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
+var video = document.querySelector(".video");
+var btnPlay = document.querySelector(".play_block");
+var btnPause = document.querySelector(".pause_block");
+var btnStop = document.querySelector(".stop_block");
 
